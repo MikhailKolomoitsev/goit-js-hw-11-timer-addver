6b8c58d0b875f8e1d0e3619767887bf76368059f
mihon.testosterone <mihon.testosterone@gmail.com>
2021-06-22 11:26:01 +0300

    commit

--- date-maker.js
+++ date-maker.js
@@ -1,3 +1,7 @@
-const dateInput=document.querySelector('.date-input')
-let todayDate = new Date().toISOString().slice(0, 10);
-dateInput.min=`${todayDate}`
+import {format} from 'date-fns'
+let  date = format(new Date(2016, 0, 1))
+console.log(date);
+
+// const dateInput=document.querySelector('.date-input')
+// let todayDate = new Date().toISOString().slice(0, 10);
+// dateInput.min=`${todayDate}`
